fdb07f2ff74368bb9fec51608836e332601070a8
Nio kasgami <devil_nicolas@hotmail.com>
2016-08-11 21:24:27 -0700

    New fix

---
+++ Scripts/Sprite_InteractiveButton.js
@@ -0,0 +1,14 @@
+﻿/**
+ * The class who hold interactive button
+ */
+
+function Sprite_InteractiveButton() {
+    this.initialize.apply(this, arguments);
+}
+Sprite_InteractiveButton.prototype.constructor = Sprite_InteractiveButton;
+Sprite_InteractiveButton.prototype = Object.create(Sprite_Button.prototype);
+
+Sprite_InteractiveButton.prototype.initialize = function (bitmap) {
+    Sprite_Button.prototype.initialize.call(this, bitmap);
+    
+};
