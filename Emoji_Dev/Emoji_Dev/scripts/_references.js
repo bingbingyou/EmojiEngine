cc51c58248764c5a9c91365f0bb59a6244e831b5
Nio kasgami <devil_nicolas@hotmail.com>
2016-08-12 01:28:51 -0700

    removed uselessFiles and libs files

--- Emoji_Dev/Emoji_Dev/scripts/_references.js
+++
@@ -1,8 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../bin/tool plugin/pluginmanagerextender.js" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="../hello.js" />
-/// <reference path="../main.js" />
-/// <reference path="dummy.js" />
-/// <reference path="intuitivemenubuttonv3.js" />
-/// <reference path="sprite_interactivebutton.js" />
