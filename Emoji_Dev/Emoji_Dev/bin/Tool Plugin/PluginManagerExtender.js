058c5c53e82924c288a91b9436ed202b389dae41
Nio Kasgami <devil_nicolas@hotmail.com>
2017-02-17 00:33:08 -0500

    Updated for add toClampedNumber and toGlobal

--- Emoji_Dev/Emoji_Dev/bin/Tool Plugin/PluginManagerExtender.js
+++ Emoji_Dev/Emoji_Dev/bin/Tool Plugin/PluginManagerExtender.js
@@ -1,4 +1,4 @@
-/*:
+//*:
  * =========================================================================
  *  ■ Basic Header
  * -------------------------------------------------------------------------
@@ -6,7 +6,7 @@
  * To be use for create plugins with more clean code.
  * <EmojiEngine>
  * @author Nio Kasgami
- * @version 3.0.0
+ * @version 4.0.0
  * @license  https://github.com/niokasgami/EmojiEngine/blob/master/LICENSE ( Under Tool Plugin license)
  * @requires nothing
  * =========================================================================
@@ -14,41 +14,17 @@
 
 'use strict';
 var imported = imported || {};
-imported.PluginManager = '3.0.0';
+imported.PluginManager = '4.0.0';
 
 //==============================================================================
 // ■ PluginManager
 //==============================================================================
 
-
-/**
- * Will convert the parameters in a Array using a list method (Only for number)
- * The format in the pluginManager is number,number,number,againANumber
- * @param {Plugin} plugin The plugin variable.
- * @param {String} parameters The parameters name in string.
- * @static
- * @deprecated since 3.00
- */
-PluginManager.toNumberList = function (plugin, parameters) {
-    return plugin[parameters].split(',').map(function (i) { return Number(i) || 0; });
-};
-
-/**
- * Will conver the parameters in a Array using a list method (Only for string)
- * The format in the pluginManager is string,string,string,againAString
- * @param {Plugin} plugin The plugin variable.
- * @param {String} parameters The parameters name in string.
- * @static
- * @deprecated since 3.00
- */
-PluginManager.toStringList = function (plugin, parameters) {
-    return plugin[parameters].split(',').map(function (i) { return String(i) || ''; });
-};
-
 /**
  * Will convert the parameters string into a boolean or make sure it's a boolean.
  * @param {Plugin} plugin The plugin variable.
  * @param {String} parameters The parameters name in string.
+ * @returns {Boolean}
  */
 PluginManager.toBoolean = function (plugin, parameters) {
     var n = plugin[parameters];
@@ -68,7 +44,9 @@ PluginManager.toBoolean = function (plugin, parameters) {
  * @param {String} plugin The id of the plugin in the format <idName>.
  */
 PluginManager.getPluginID = function (plugin) {
-    return $plugins.filter(function (p) { return p.description.contains('<' + plugin + '>'); })[0].parameters;
+    return $plugins.filter(function (p) {
+        return p.description.contains('<' + plugin + '>');
+    })[0].parameters;
 };
 
 /**
@@ -95,7 +73,7 @@ PluginManager.toBitmap = function (plugin, parameters, folder) {
  * @param {String} parameters
  * @returns {Number}
  */
-PluginManager.toNumber = function (plugin, parameters) { 
+PluginManager.toNumber = function (plugin, parameters) {
     return Number(plugin[parameters])
 };
 
@@ -107,8 +85,8 @@ PluginManager.toNumber = function (plugin, parameters) {
  * @param {String} parameters
  * @returns {Number}
  */
-PluginManager.toProbability = function(plugin, parameters){
-    return Number(plugin[parameters]).clamp(0,1);
+PluginManager.toProbability = function (plugin, parameters) {
+    return Number(plugin[parameters]).clamp(0, 1);
 };
 /**
  * Will convert the parameters into ANY JS legal Array.
@@ -120,21 +98,50 @@ PluginManager.toProbability = function(plugin, parameters){
  * @returns {Array<any>} 
  */
 Pluginmanager.toArray = function (plugin, parameters) {
-  var array ;
-  try {
-     array = JSON.parse(plugin[parameters]);
-  } catch (e) {
-     try {
-         array = JSON.parse('[' + plugin[parameters] + ']');
-     } catch (e) {
-         throw  "Parameter " + parameters + " is not a valid array";
-     }
-  }
-  if (!array instanceof Array) {
-    throw "Parameter " + parameters + " is not a valid array";
-  }
-  return array;
-}
+    var array;
+    try {
+        array = JSON.parse(plugin[parameters]);
+    } catch (e) {
+        try {
+            array = JSON.parse('[' + plugin[parameters] + ']');
+        } catch (e) {
+            throw "Parameter " + parameters + " is not a valid array";
+        }
+    }
+    if (!array instanceof Array) {
+        throw "Parameter " + parameters + " is not a valid array";
+    }
+    return array;
+};
+
+/**
+ * Will return a number clamped to a specific value. Useful if you want to 
+ * lock the parameters to a certain range.
+ * @static
+ * @param {PluginManager} plugin the plugin variable
+ * @param {String} parameters the parameters String
+ * @param {Number} min the minimal value for the clamp
+ * @param {Number} max the maximum value for the clamp
+ * @returns {Number}
+ */
+PluginManager.toClampedNumber = function (plugin, parameters, min, max) {
+    return Number(plugin[parameters]).clamp(min, max);
+};
+
+/**
+ * Will return a global variable or object. it's will return any object
+ * as long it's in the Global space. Useful for MV global variables.
+ * @static
+ * @param {PluginManager} plugin
+ * @param {String} parameters
+ */
+PluginManager.toGlobal = function (plugin, parameters) {
+    if (plugin[parameters] in window) {
+        return window[plugin[parameters]];
+    } else {
+        throw new Error(parameters + 'is undefined or out of the global scope.');
+    }
+};
 
 //===============================================================================
 // => END : Emoji
@@ -144,17 +151,4 @@ Pluginmanager.toArray = function (plugin, parameters) {
 /**
  * Deprecated Members.
  */
-Object.defineProperties(PluginManager,
-{
-    toNumberList: {
-        get: function () {
-            console.warn('PluginManager.toNumberList is deprecated and will be removed in the next update. Please use toArray instead.');
-        }
-    },
-
-    toStringList: {
-        get function() {
-            console.warn('PluginManager.toStringList is deprecated and will be removed in the next update. Please use toArray instead.');
-        }
-    }
-});
+Object.defineProperties(PluginManager, {});
